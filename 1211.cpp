81da973abe116b190df4a29b812f6cee540cd6b8
Vova Korginzkyy <kyper32@ya.ru>
2013-05-29 11:14:57 +0300

    Create 1211.cpp

---
+++ 1211.cpp
@@ -0,0 +1,37 @@
+// 1211.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include "join.h"
+#include "Rational.h"
+
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+    setlocale(0, "Russian");
+  Rational myRational;
+	double t,m,n;
+	
+	cout<<"Введите Числитель: ";
+	cin>>t;
+	cout<<endl;
+	
+	cout<<"Введите знаменатель 1: ";
+	cin>>m;
+	cout<<endl;
+	
+	cout<<"Введите знаменатель 2: ";
+	cin>>n;
+	cout<<endl;
+	
+	myRational.setChislitel(t);
+	myRational.setZnamenatel(m);
+	myRational.setZmamenatel(n);
+	
+	myRational.displayMessage();
+	cout<<endl;
+	_getch();
+	return 0;
+}
+
