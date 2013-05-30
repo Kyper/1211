8204783e202c76a7b3f368607fe34e4774ef75e7
Vova Korginzkyy <kyper32@ya.ru>
2013-05-30 13:40:49 +0300

    Update 1211.cpp

--- 1211.cpp
+++ 1211.cpp
@@ -9,26 +9,31 @@
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-    setlocale(0, "Russian");
-  Rational myRational;
+	setlocale(0, "Russian");
+	Rational myRational;
 	double t,m,n;
-	
+
 	cout<<"Введите Числитель: ";
 	cin>>t;
 	cout<<endl;
-	
+
 	cout<<"Введите знаменатель 1: ";
 	cin>>m;
 	cout<<endl;
-	
+
 	cout<<"Введите знаменатель 2: ";
 	cin>>n;
 	cout<<endl;
-	
+
 	myRational.setChislitel(t);
 	myRational.setZnamenatel(m);
 	myRational.setZmamenatel(n);
-	
+
+	myRational.setSumma(t+m);
+	myRational.setDelenie(t/m);
+	myRational.setMinus(t-m);
+	myRational.setUmnosh(t*m);
+
 	myRational.displayMessage();
 	cout<<endl;
 	_getch();
