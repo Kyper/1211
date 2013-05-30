ffbc778bdbf752da549c56a01b825c5710f133f4
Vova Korginzkyy <kyper32@ya.ru>
2013-05-30 13:40:27 +0300

    Update Rational.h

--- Rational.h
+++ Rational.h
@@ -4,8 +4,8 @@
 class Rational
 {
 public:
-  Rational();
-	
+	Rational();
+
 	Rational(double, double, double);
 	void setChislitel(double);
 	double getChislitel();
@@ -15,8 +15,23 @@ public:
 	double getZmamenatel();
 	void displayMessage();
 
+	Rational(double, double, double, double);
+	double getSumma();
+	void setSumma(double);
+	double getDelenie();
+	void setDelenie(double);
+	double getMinus();
+	void setMinus(double);
+	double getUmnosh();
+	void setUmnosh(double);
+
 private:
 	double Chislitel;
 	double Znamenatel;
 	double Zmamenatel;
+	double Summa;
+	double Delenie;
+	double Minus;
+	double Umnosh;
+
 };
