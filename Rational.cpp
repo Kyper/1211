af719cd120da1b5142e0612ddfcace479f862c30
Vova Korginzkyy <kyper32@ya.ru>
2013-05-30 13:39:55 +0300

    Update Rational.cpp

--- Rational.cpp
+++ Rational.cpp
@@ -3,19 +3,71 @@
 #include "Rational.h"
 
 
-    Rational::Rational(){}
-  void Rational::displayMessage()
+    Rational::Rational()
+	{
+	}
+	void Rational::displayMessage()
 	{
 		cout<<"Числитель: "<<getChislitel()<<endl;
 		cout<<"Знаменатель 1: "<<getZnamenatel()<<endl;
 		cout<<"Знаменатель 2: "<<getZmamenatel()<<endl;
-
+		
+		cout<<endl;
+		
+		cout<<"Cумма: "<<getSumma()<<endl;
+		cout<<"Деление: "<<getDelenie()<<endl;
+		cout<<"Минус: "<<getMinus()<<endl;
+		cout<<"Умножение: "<<getUmnosh()<<endl;
+		
+		cout<<endl;
+		
+		cout<<"В виде дроби: "<<getChislitel()<<"/"<<getZnamenatel();
 	}
 	Rational::Rational(double chis, double znam, double zmam)
 	{
 		setChislitel(chis);
 		setZnamenatel(znam);
 		setZmamenatel(zmam);
+	}
+	/*------------Действия---------*/
+	Rational::Rational(double sum, double del, double min, double um)
+	{
+		setSumma(sum);
+		setDelenie(del);
+		setMinus(min);
+		setUmnosh(um);
+	}
+	void Rational::setSumma(double sum)
+	{
+		Summa=sum;
+	}
+	double Rational::getSumma()
+	{
+		return Summa;
+	}
+	void Rational::setDelenie(double del)
+	{
+		Delenie=del;
+	}
+	double Rational::getDelenie()
+	{
+		return Delenie;
+	}
+	void Rational::setMinus(double min)
+	{
+		Minus=min;
+	}
+	double Rational::getMinus()
+	{
+		return Minus;
+	}
+	void Rational::setUmnosh(double um)
+	{
+		Umnosh=um;
+	}
+	double Rational::getUmnosh()
+	{
+		return Umnosh;
 	}
 	/*----------Chislitel----------*/
 	void Rational::setChislitel(double chis)
